e892ad814585c76e651ff4bd386b425a7e82730f
Leonardo Lopes <leonardolivelopes2@gmail.com>
2023-12-14 10:47:43 -0300

    select database

--- src/main/java/aplicacao/App.java
+++ src/main/java/aplicacao/App.java
@@ -7,20 +7,24 @@ import javax.persistence.Persistence;
 
 public class App {
 	public static void main(String[] args) {
-		Pessoa pessoa = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa pessoa2 = new Pessoa(null, "Joaquim torres", "carlos@gmail.com");
-		Pessoa pessoa3 = new Pessoa(null, "Ana maria", "carlos@gmail.com");
-		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager(); 
+//		Pessoa pessoa = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+//		Pessoa pessoa2 = new Pessoa(null, "Joaquim torres", "carlos@gmail.com");
+//		Pessoa pessoa3 = new Pessoa(null, "Ana maria", "carlos@gmail.com");
+//		
+//
+//		em.getTransaction().begin();
+//		em.persist(pessoa);
+//		em.persist(pessoa2);
+//		em.persist(pessoa3);
+//		em.getTransaction().commit();
 
-		em.getTransaction().begin();
-		em.persist(pessoa);
-		em.persist(pessoa2);
-		em.persist(pessoa3);
-		em.getTransaction().commit();
-		
+		Pessoa p = em.find(Pessoa.class, 2);
+		System.out.println(p);
 		System.out.println("Pronto");
+		em.close();
+		emf.close();
 		
 	}
 }
