1b3e2842c95b02dd96b29592544d5d6a1c1ec0fa
Leonardo Lopes <leonardolivelopes2@gmail.com>
2023-12-14 10:45:40 -0300

    insert database

--- src/main/java/aplicacao/App.java
+++ src/main/java/aplicacao/App.java
@@ -1,15 +1,26 @@
 package aplicacao;
 
 import dominio.Pessoa;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class App {
 	public static void main(String[] args) {
-		Pessoa pessoa = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa pessoa2 = new Pessoa(2, "Joaquim torres", "carlos@gmail.com");
-		Pessoa pessoa3 = new Pessoa(3, "Ana maria", "carlos@gmail.com");
-		System.out.println(pessoa);
-		System.out.println(pessoa2);
-		System.out.println(pessoa3);
+		Pessoa pessoa = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa pessoa2 = new Pessoa(null, "Joaquim torres", "carlos@gmail.com");
+		Pessoa pessoa3 = new Pessoa(null, "Ana maria", "carlos@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager(); 
+
+		em.getTransaction().begin();
+		em.persist(pessoa);
+		em.persist(pessoa2);
+		em.persist(pessoa3);
+		em.getTransaction().commit();
+		
+		System.out.println("Pronto");
 		
 	}
 }
